30c1c917e6764a21ba33aa79dcf5d3859574b189
slipperyNipple <slipperyNipple@flenstad.com>
2020-04-24 10:13:35 +0200

    changed url

--- client/deliver.py
+++ client/deliver.py
@@ -2,5 +2,5 @@ from MonitorClient.Deliver import Deliver
 import json
 
 if __name__ == '__main__':
-    d = Deliver('http://127.0.0.1:5000/')
+    d = Deliver('https://api.aroonie.com/')
     d.upload()
